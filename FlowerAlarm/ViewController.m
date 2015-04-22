5829f34f3dfe0e6a9d8a369e762ebdcee405ea09
Andreas Buff <andreas.buff@cgm.com>
2015-04-22 13:26:16 +0200

    Initial project

---
+++ FlowerAlarm/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FlowerAlarm
+//
+//  Created by Andreas Buff on 22/04/15.
+//  Copyright (c) 2015 CockieMonster. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
